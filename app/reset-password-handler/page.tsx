a1e1b61ef85995b7f0c5ffbb20098e9d7aada3e6
AbdallaEmadEldinAbdalla <abdalla.emad.eldin@gmail.com>
2025-05-14 23:33:30 +0400

    .

--- app/reset-password-handler/page.tsx
+++ app/reset-password-handler/page.tsx
@@ -3,11 +3,11 @@
 import { useEffect } from "react";
 
 export default function ResetPasswordHandler() {
-  useEffect(() => {
-    const fragment = window.location.hash.substring(1);
-    const url = `https://dashboard.bexpo.xyz/reset-password?${fragment}`;
-    window.location.href = url;
-  }, []);
+  // useEffect(() => {
+  //   const fragment = window.location.hash.substring(1);
+  //   const url = `https://dashboard.bexpo.xyz/reset-password?${fragment}`;
+  //   window.location.href = url;
+  // }, []);
 
   return <div />;
 }
